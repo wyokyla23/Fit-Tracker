6f99d0d9682add7715b365fec804931ca2d6b29c
wyokyla23 <wyokyla23@gmail.com>
2020-04-30 12:41:42 -0500

    Created Project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// <Router>
+//   <Theme>
+//     <Container>
+//       <Header>
+//       </Header>
+//       <Routes/>
+//     </Container>
+//   </Theme>
+// </Router>
+function App() {
+  return <div>Hello</div>;
+}
+
+export default App;
---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const fitPurple = "#292659";
+const fitLightPurple = "#595688";
+
+export default createMuiTheme({
+  palette: {
+    common: {
+      purple: `${fitPurple}`,
+      lightPurple: `${fitLightPurple}`,
+    },
+  },
+});
