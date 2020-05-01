d574462054eb4baf6d2d858871ff80c3d9d696b7
wyokyla23 <wyokyla23@gmail.com>
2020-05-01 12:45:57 -0500

    Created Header layout

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -8,6 +8,7 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Grid from "@material-ui/core/Grid";
+import { useTheme } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,65 +19,106 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
+    fontSize: "2.5rem",
+    fontFamily: "roboto",
+    [theme.breakpoints.down("md")]: {
+      fontSize: "1.5rem",
+    },
   },
   buttonContainer: {
     justifyContent: "flex-end",
+  },
+  appBar: {
+    backgroundColor: theme.palette.common.purple,
+  },
+  toolBar: {
+    minHeight: "90px",
+  },
+  gridItem: {
+    flexGrow: 2,
+  },
+  iconTitleContainer: {
+    justifyContent: "flex-start",
   },
 }));
 
 export default function Header() {
   const classes = useStyles();
+  const theme = useTheme();
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
+      <AppBar
+        position="static"
+        className={classes.appBar}
+      >
+        <Toolbar className={classes.toolBar}>
+          <Grid
+            container
+            className={classes.iconTitleContainer}
           >
-            <MenuIcon />
-          </IconButton>
-          <Button
-            component={Link}
-            to="./home-page"
-            disableRipple
-            color="inherit"
-          >
-            <Typography
-              variant="h6"
-              className={classes.title}
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
             >
-              Fit Tracker
-            </Typography>
-          </Button>
+              <MenuIcon />
+            </IconButton>
+            <Button
+              component={Link}
+              to="./home-page"
+              disableRipple
+              color="inherit"
+            >
+              <Typography
+                variant="h6"
+                className={classes.title}
+              >
+                Fit Tracker
+              </Typography>
+            </Button>
+          </Grid>
           <Grid
             container
             className={classes.buttonContainer}
           >
-            <Button
-              component={Link}
-              to="./home-page"
-              color="inherit"
+            <Grid
+              item
+              className={classes.gridItem}
             >
-              Home
-            </Button>
-            <Button
-              component={Link}
-              to="./profile-page"
-              color="inherit"
+              <Button
+                component={Link}
+                to="./home-page"
+                color="inherit"
+              >
+                Home
+              </Button>
+            </Grid>
+            <Grid
+              item
+              className={classes.gridItem}
             >
-              Profile
-            </Button>
-            <Button
-              component={Link}
-              to="./login-page"
-              color="inherit"
+              <Button
+                component={Link}
+                to="./profile-page"
+                color="inherit"
+              >
+                Profile
+              </Button>
+            </Grid>
+            <Grid
+              item
+              className={classes.gridItem}
             >
-              Login
-            </Button>
+              <Button
+                component={Link}
+                to="./login-page"
+                color="inherit"
+              >
+                Login
+              </Button>
+            </Grid>
           </Grid>
         </Toolbar>
       </AppBar>
